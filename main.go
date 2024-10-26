36192902704e2d8c6522b8000dd3475225982170
15683 <15683@mail.ru>
2024-10-26 20:11:39 +0300

    continue project

--- main.go
+++ main.go
@@ -1,5 +1,83 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
 
+	"github.com/emersion/go-smtp"
+)
+
+func main() {
+	s := smtp.NewServer(&Backend{})
+
+	s.Addr = ":2525"
+	s.Domain = "localhost"
+	s.WriteTimeout = 10 * time.Second
+	s.ReadTimeout = 10 * time.Second
+	s.MaxMessageBytes = 1024 * 1024
+	s.MaxRecipients = 50
+	s.AllowInsecureAuth = true
+
+	log.Println("Starting server at", s.Addr)
+	if err := s.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Backend реализация SMTP сервера
+type Backend struct{}
+
+func (bkd *Backend) NewSession(_ *smtp.Conn) (smtp.Session, error) {
+	return &Session{}, nil
+}
+
+// Session объект создается после команды EHLO
+type Session struct{}
+
+// Теперь реализуем методы Session
+func (s *Session) Mail(from string, opts *smtp.MailOptions) error {
+	fmt.Println("Mail from:", from)
+	s.From = from
+	return nil
+}
+
+func (s *Session) Rcpt(to string) error {
+	fmt.Println("Rcpt to:", to)
+	s.To = append(s.To, to)
+	return nil
+}
+
+func (s *Session) Data(r io.Reader) error {
+	if b, err := io.ReadAll(r); err != nil {
+		return err
+	} else {
+		fmt.Println("Received message:", string(b))
+
+		// Тут происходит обработка письма
+		return nil
+	}
+}
+
+func (s *Session) AuthPlain(username, password string) error {
+	if username != "testuser" || password != "testpass" {
+		return fmt.Errorf("Invalid username or password")
+	}
+
+	return nil
+}
+
+func (s *Session) Logout() error {
+	return nil
+}
+
+func lookupMX(domain string) ([]*net.MX, error) {
+	mxRecords, err := net.LookupMX(domain)
+	if err != nil {
+		return nil, fmt.Errorf("Error looking up MX records: %v", err)
+	}
+
+	return mxRecords, nil
 }
